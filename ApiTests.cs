5c950083815ab4f7f238a56f287d546b2e1480fb
Pau8oq <40770303+Pau8oq@users.noreply.github.com>
2024-01-17 22:35:12 +0200

    fsdf

--- ApiTests.cs
+++ ApiTests.cs
@@ -7,15 +7,23 @@ namespace TeamCityWithGitHub.Test
     public class ApiTests
     {
         [TestMethod]
-        public async Task DefaultRoute_ReturnsHelloWorld()
+        public void DefaultRoute_ReturnsHelloWorld()
         {
-            var webAppFactory = new WebApplicationFactory<Program>();
-            var httpClient = webAppFactory.CreateDefaultClient();
+            //var webAppFactory = new WebApplicationFactory<Program>();
+            //var httpClient = webAppFactory.CreateDefaultClient();
 
-            var response = await httpClient.GetAsync("WeatherForecast/HelloWorld");
-            var stringResult = await response.Content.ReadAsStringAsync();
+            //var response = await httpClient.GetAsync("WeatherForecast/HelloWorld");
+            //var stringResult = await response.Content.ReadAsStringAsync();
 
-            Assert.AreEqual("Hello World!", stringResult);
+           // Assert.AreEqual("Hello World!", stringResult);
+        }
+
+
+        [TestMethod]
+        public void Test_Failure()
+        {
+            var str = "not hello";
+            Assert.AreEqual(str, "hello");
         }
     }
 }
--- obj/Debug/net8.0/TeamCityWithGitHub.Test.AssemblyInfo.cs
+++ obj/Debug/net8.0/TeamCityWithGitHub.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamCityWithGitHub.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776140f53f3c5ee3385463e42bccb585dfa902b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a4378c7a118f751c4da96abb93a929359ff931a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamCityWithGitHub.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamCityWithGitHub.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
