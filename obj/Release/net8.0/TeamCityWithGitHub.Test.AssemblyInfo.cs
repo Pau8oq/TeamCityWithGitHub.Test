6a4378c7a118f751c4da96abb93a929359ff931a
Pau8oq <40770303+Pau8oq@users.noreply.github.com>
2024-01-17 21:31:32 +0200

    add reference

--- obj/Release/net8.0/TeamCityWithGitHub.Test.AssemblyInfo.cs
+++ obj/Release/net8.0/TeamCityWithGitHub.Test.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamCityWithGitHub.Test")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776140f53f3c5ee3385463e42bccb585dfa902b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamCityWithGitHub.Test")]
